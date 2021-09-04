0296e682c103037bc7820eff3f44a68495909414
morriskim <morriskim@kakao.com>
2021-09-04 23:15:30 +0900

    logging 처리 하기

--- src/main/java/com/morris/logging/controller/LoggingController.java
+++ src/main/java/com/morris/logging/controller/LoggingController.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @RestController
 @Slf4j
 public class LoggingController {
@@ -19,5 +22,20 @@ public class LoggingController {
         log.debug(body);
         log.trace(body);
         return new ResponseEntity( body,HttpStatus.OK);
+    }
+
+    @GetMapping("/exception")
+    public ResponseEntity<?> exeception() {
+        String body = "Exception";
+        try {
+            throw new RuntimeException("Morris Throw Exception ");
+        }catch(Exception exception) {
+            String errorStack = Arrays.stream(exception.getStackTrace()).map((e) -> {
+                return e.toString();
+            }).collect(Collectors.joining("\\r\\n"));
+            log.error("Morris ExceptionMsg :"+exception.getMessage()+", MorrisExceptionStack : "+errorStack);
+            return new ResponseEntity( exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
